93f5e3f629b75cd4bf6b22ff4a406ab14be2edae
athirson <athirsonarceus@gmail.com>
2022-09-01 15:10:25 -0300

    more subqueries for Invetory database

--- dbInventory/subqueries.sql
+++ dbInventory/subqueries.sql
@@ -35,3 +35,18 @@ SELECT tbProduto.descricaoProduto, tbProduto.valorProduto
 FROM tbProduto
 	WHERE tbProduto.valorProduto IN (SELECT MIN(tbProduto.valorProduto) FROM tbProduto)
  
+----- Another one
+-- 1.
+SELECT tbProduto.codProduto, tbProduto.descricaoProduto, tbFabricante.nomeFabricante FROM tbProduto
+	INNER JOIN tbFabricante ON tbProduto.codFabricante = tbFabricante.codFabricante
+	WHERE tbProduto.valorProduto = (SELECT MAX(tbProduto.valorProduto) FROM tbProduto)
+
+-- 2.
+SELECT tbProduto.valorProduto, tbProduto.descricaoProduto, tbFabricante.nomeFabricante FROM tbProduto
+	INNER JOIN tbFabricante ON tbProduto.codFabricante = tbFabricante.codFabricante
+	WHERE tbProduto.valorProduto >= (SELECT AVG(tbProduto.valorProduto) FROM tbProduto)
+
+-- 3.
+SELECT tbCliente.nomeCliente FROM tbCliente
+INNER JOIN tbVenda ON tbCliente.codCliente = tbVenda.codCliente
+	WHERE tbVenda.valorTotalVenda >= (SELECT AVG(tbVenda.valorTotalVenda) FROM tbVenda)
