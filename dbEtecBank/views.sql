fbbdf6f1820dee1c323eb6d5c1d1f5671860a930
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2022-09-30 12:53:39 -0300

    vwCorrentePoupanca

--- dbEtecBank/views.sql
+++ dbEtecBank/views.sql
@@ -1,24 +1,42 @@
 use EtecBank
 
-create view vwTotalBalance as
+create view vwSaldoTotal as
 	select dbo.accountHolder.name holder, dbo.checkingAccount.balance ccBalance, dbo.savingAccount.balance sBalance, dbo.savingAccount.balance + dbo.savingAccount.balance sumBalance
 	from dbo.accountHolder
 		inner join dbo.checkingAccount on dbo.accountHolder.id = dbo.checkingAccount.accountHolderID
 		inner join dbo.savingAccount on dbo.checkingAccount.accountHolderID = dbo.savingAccount.accountHolderID
 
-select * from vwTotalBalance order by holder ASC
+select * from vwSaldoTotal order by holder ASC
 
-create view vwSavingBalanceProjection as
+create view vwProjecaoSaldoPoupanca as
 	select dbo.accountHolder.name holder, dbo.savingAccount.balance / 0.5 projection
 		from dbo.accountHolder
 			inner join dbo.savingAccount on dbo.accountHolder.id = dbo.savingAccount.accountHolderID
 
-select * from vwSavingBalanceProjection
+select * from vwProjecaoSaldoPoupanca
 
-create view vwShowHolder as
+create view vwExibeCorrentista as
 	select dbo.accountHolder.name holder, dbo.checkingAccount.agencyNumber, dbo.checkingAccount.number, dbo.checkingAccount.digit, dbo.checkingAccount.balance, dbo.checkingAccount.createdAt, dbo.checkingAccount.accountHolderID
 	from dbo.accountHolder
 		inner join dbo.checkingAccount on dbo.accountHolder.id = dbo.checkingAccount.accountHolderID
 
-select * from vwShowHolder
+select * from vwExibeCorrentista
 
+create view vwCorrentistaConta as
+	select dbo.accountHolder.name, dbo.checkingAccount.number, dbo.checkingAccount.balance
+	from dbo.accountHolder
+		inner join dbo.checkingAccount on dbo.accountHolder.id = dbo.checkingAccount.accountHolderID 
+			where dbo.checkingAccount.balance > 500000.00
+			
+select * from vwCorrentistaConta
+
+create view vwCorrentePoupança as
+	select dbo.accountHolder.name, dbo.accountHolder.cpf, dbo.checkingAccount.balance ccBalance, dbo.savingAccount.balance saBalance
+	from dbo.accountHolder	
+		inner join dbo.checkingAccount on dbo.accountHolder.id = dbo.checkingAccount.accountHolderID
+		inner join dbo.savingAccount on dbo.savingAccount.accountHolderID = dbo.checkingAccount.accountHolderID
+
+select * from vwCorrentePoupança
+
+
+		                     
